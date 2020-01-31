269b1df62f58dbec91ed8d40c27431da3c992497
Hunter Wheat <elitewheatproductionz@gmail.com>
2020-01-31 12:10:31 -0800

    beautify

--- index.php
+++ index.php
@@ -1,60 +1,57 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<?php
-$thc = $_POST["thc"];    
-$m = $_POST["mass"];  
-$unit = $_POST["unit"];    
-$cost = $_POST["cost"];
-?>
-
-<h2>BudCalc <img src='leaf.png' style='float: right;'/></h2>
-<p>This nifty calculator will help you determine the cost of your weed in dollars/gram THC</p>
-<div>
-<form method="POST">
-    <p>
-        <label>
-            THC Percentage:<br>
-            <input type="number" step=.01 name="thc" value=""> %<br>
-            Weight/Mass:<br>
-            <input type="number" step=.001 name="mass" value=""><br>
-            <input type="radio" name="unit" value="g" checked=true><label class="radio"> Grams</label><br>
-            <input type="radio" name="unit" value="oz"><label class="radio"> Ounces</label><br>
-            Cost:<br>
-            <input type="number" name="cost" value="">
-        </label>
-        <br><input type="submit" value="Calculate">
-    </p>
-<div id="box">
-    <p id="result"><img src='img.png' style='float: right;'/></p>
-</div>
-</form>
-</div>
-
-<script>
-window.onload = Main;
-function Calculate(thc, m, unit, cost)
-{
-    m_ = m;
-    if (unit == "oz")
-    {
-        m_ *= 28.34952;     
-    }
-    return cost/((thc/100)*m_);
-}
-function Main(object)
-{
-    thc = "<?php echo $thc ?>"; 
-    m = "<?php echo $m ?>"; 
-    unit = "<?php echo $unit ?>";
-    cost = "<?php echo $cost ?>";
-    parent = document.getElementById("result");
-    parent.insertBefore(document.createTextNode(Math.round(Calculate(thc, m, unit, cost)*100)/100),parent.childNodes[0]);
-}
-</script>
-
-</body>
+   <head>
+      <link rel="stylesheet" type="text/css" href="style.css">
+   </head>
+   <body>
+      <?php
+         $thc = $_POST["thc"];    
+         $m = $_POST["mass"];  
+         $unit = $_POST["unit"];    
+         $cost = $_POST["cost"];
+         ?>
+      <h2>BudCalc <img src='leaf.png' style='float: right;'/></h2>
+      <p>This nifty calculator will help you determine the cost of your weed in dollars/gram THC</p>
+      <div>
+         <form method="POST">
+            <p>
+               <label>
+               THC Percentage:<br>
+               <input type="number" step=.01 name="thc" value=""> %<br>
+               Weight/Mass:<br>
+               <input type="number" step=.001 name="mass" value=""><br>
+               <input type="radio" name="unit" value="g" checked=true><label class="radio"> Grams</label><br>
+               <input type="radio" name="unit" value="oz"><label class="radio"> Ounces</label><br>
+               Cost:<br>
+               <input type="number" name="cost" value="">
+               </label>
+               <br><input type="submit" value="Calculate">
+            </p>
+            <div id="box">
+               <p id="result"><img src='img.png' style='float: right;'/></p>
+            </div>
+         </form>
+      </div>
+      <script>
+         window.onload = Main;
+         function Calculate(thc, m, unit, cost)
+         {
+             m_ = m;
+             if (unit == "oz")
+             {
+                 m_ *= 28.34952;     
+             }
+             return cost/((thc/100)*m_);
+         }
+         function Main(object)
+         {
+             thc = "<?php echo $thc ?>"; 
+             m = "<?php echo $m ?>"; 
+             unit = "<?php echo $unit ?>";
+             cost = "<?php echo $cost ?>";
+             parent = document.getElementById("result");
+             parent.insertBefore(document.createTextNode(Math.round(Calculate(thc, m, unit, cost)*100)/100),parent.childNodes[0]);
+         }
+      </script>
+   </body>
 </html>
